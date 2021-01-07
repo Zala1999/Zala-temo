89e1035726d2fc6ee4f7574646278bdb4edda6ea
yuvrajsinh zala <75449804+Zala1999@users.noreply.github.com>
2021-01-07 04:44:48 +0300

    Add files via upload

---
+++ project-18-task-5.c
@@ -0,0 +1,33 @@
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+int sortExceptK(int arr[], int k, int n) 
+{ 
+	// Move k-th element to end 
+	swap(arr[k], arr[n-1]); 
+
+	// Sort all elements except last 
+	sort(arr, arr + n - 1); 
+
+	// Store last element (originally k-th) 
+	int last = arr[n-1]; 
+
+	// Move all elements from k-th to one 
+	// position ahead. 
+	for (int i=n-1; i>k; i--) 
+	arr[i] = arr[i-1]; 
+
+	// Restore k-th element 
+	arr[k] = last; 
+} 
+int main() 
+{ 
+	int a[] = {10, 4, 11, 7, 6, 20 }; 
+	int k = 2; 
+	int n = sizeof(a) / sizeof(a[0]); 
+	sortExceptK(a, k, n); 
+	for (int i = 0; i < n; i++) 
+		cout << a[i] << " "; 
+}
+
